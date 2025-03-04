3d7bbda1a7836ce9a6d8f7caa1e1a418e2a07e54
xiao_wine <xiaowine0@gmail.com>
2025-03-04 23:27:18 +0800

    分离样式css，添加电感计算，更新about页面

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,22 @@
  * 如需了解更多开发细节，请阅读：
  * https://prodocs.lceda.cn/cn/api/guide/
  */
-import * as extensionConfig from '../extension.json';
+// import * as extensionConfig from '../extension.json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function activate(status?: 'onStartupFinished', arg?: string): void {}
 
-export function about(): void {
-	eda.sys_MessageBox.showInformationMessage(
-		eda.sys_I18n.text('AboutDescription', undefined, undefined, extensionConfig.version),
-		eda.sys_I18n.text('About'),
-	);
+export function open_page_about(): void {
+	eda.sys_IFrame.openIFrame('/iframe/about.html', 560, 330, 'about');
 }
-export function line_width_calculation(): void {
-	eda.sys_IFrame.openIFrame('/iframe/line-width.html', 560, 365, 'line-width');
+export function open_page_line_width_calculation(): void {
+	eda.sys_IFrame.openIFrame('/iframe/line-width.html', 560, 400, 'line-width');
 }
-export function resistive_divider_calculation(): void {
-	eda.sys_IFrame.openIFrame('/iframe/resistive-divider.html', 560, 575, 'resistive-divider');
+export function open_page_resistive_divider_calculation(): void {
+	eda.sys_IFrame.openIFrame('/iframe/resistive-divider.html', 560, 610, 'resistive-divider');
+}
+export function open_page_inductor_design(): void {
+	eda.sys_IFrame.openIFrame('/iframe/inductor-design.html', 664, 664, 'inductor-design');
 }
 export function go_project(): void {
 	eda.sys_Window.open('https://github.com/xiaowine/pcb-calculation-tools');
